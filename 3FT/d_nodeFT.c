79b675654a6da92d28cc432e20caf8b98a888ac7
Judah Guggenheim <judahguggenheim@dynamic-oit-vapornet100-10-9-23-50.princeton.edu>
2023-04-10 21:48:46 -0400

    upload of 3FT  - take 1

---
+++ 3FT/d_nodeFT.c
@@ -0,0 +1,176 @@
+/*--------------------------------------------------------------------*/
+/* d_nodeDT.h                                                         */
+/* Author: Judah Guggenheim, code borrowed from Christopher Moretti   */
+/*--------------------------------------------------------------------*/
+
+/* NOTE: This was originally set up to be the node implementation only 
+for nodes represanting directories. I (think I) am in the process of changing that now
+to be one node that does both directories and files. */
+#include "d_nodeFT.h"
+#include "dynarray.h"
+
+/* A node representing a directory in a File Tree */
+struct d_node{
+    /* the path to this directory */
+    Path_T oPPath; /* or should it  be: Path_T *opPath;   ?? */
+
+    /* pointer to the parent (directory node) of this node */
+    dNode_T oNParent;
+
+    /* a pointer to the dynarray containing this node's directory children */
+    DynArray_T oDChildren;
+
+    /* a pointer to the dynarray containing this node's file children */
+    DynArray_T oFChildren;
+}
+
+/* maybe need an error here if trying to make a node without parents but there's already a root in the tree */
+int dNode_new(Path_T oPPath, dNode_T oNParent, dNode_T *poNResult){
+    size_t depth = Path_getDepth(oPPath);
+    Path_T parentPath;
+    size_t *uIndex;
+
+    if(depth == 0) {
+        *poNResult = NULL;
+        fprintf(stderr, "can't put a node at an empty path");
+        return NO_SUCH_PATH;
+    }
+    if(oNParent == NULL && depth != 1){
+        *poNResult = NULL;
+        fprintf(stderr, "can't put a (non-root) node at a NULL parent");
+        return NO_SUCH_PATH;
+    }
+    
+
+    /*Not sure about this one*/
+    if(oNParent!=NULL){
+    Path_prefix(oPPath, (depth-1), parentPath);
+    if(Path_comparePath(oNParent->oPPath, parentPath)!=0){
+        *poNResult = NULL;
+        fprintf(stderr, "oNParent's path is not oPPath's direct parent");
+        return NO_SUCH_PATH;
+    }
+    }
+
+    (*poNResult) = (dNode_T) malloc(sizeof(struct d_node));
+    if(*poNResult == NULL){
+        fprintf(stderr, "not enough memory to add this node");
+        return MEMORY_ERROR;
+    }
+
+    if(oNParent!=NULL){
+    if(DynArray_bsearch(oNParent->oDChildren, (*poNResult), uIndex, dNode_compare)==1){
+        fprintf(stderr, "this node already exists");
+        return ALREADY_IN_TREE;
+    }
+
+    if(DynArray_addAt(oNParent->oDChildren, uIndex, (*poNResult)) == 0){
+        fprintf(stderr, "not enough memory to add this node");
+        *poNResult = NULL;
+        return MEMORY_ERROR;
+    }
+    }
+    (*poNResult)->oPPath = oPPath;
+    (*poNResult)->oNParent = oNParent;
+    (*poNResult)->oDChildren = DynArray_new(0);
+    return SUCCESS;
+}
+/* think i'm done */
+
+/*
+  Destroys and frees all memory allocated for the subtree rooted at
+  oNNode, i.e., deletes this node and all its descendents. Returns the
+  number of nodes deleted.
+*/
+size_t dNode_free(dNode_T oNNode){
+    size_t deleteCount = 0;
+    while(DynArray_getLength(oNNode->oDChildren)!=0){
+
+    }
+    free(oNNode->oDChildren);
+    free(oNNode->oNParent);
+    free(oNNode->oPPath);
+    free(oNNode);
+}
+
+/* Returns the path object representing oNNode's absolute path. */
+Path_T dNode_getPath(dNode_T oNNode){
+    return oNNode->oPPath;
+}
+
+/*
+  Returns TRUE if oNParent has a child with path oPPath. Returns
+  FALSE if it does not.
+
+  If oNParent has such a child, stores in *pulChildID the child's
+  identifier (as used in Node_getChild). If oNParent does not have
+  such a child, stores in *pulChildID the identifier that such a
+  child _would_ have if inserted.
+
+  [Judah's notes] I don't think this is necessary. Later: ok wait yeah it is. 
+*/
+boolean dNode_hasChild(dNode_T oNParent, Path_T oPPath,
+                         size_t *pulChildID){
+                            /* This is an issue bc it takes memory */
+                            dNode_T temp;
+                            boolean bool;
+                            dNode_new(oPPath, oNParent, temp);
+                            if(DynArray_bsearch(oNParent->oDChildren, temp, pulChildID, dNode_compare())==0){bool = FALSE;}
+                            else {bool = TRUE;}
+                            dNode_free(temp);
+                            return bool;
+                         };
+/* think i'm done */
+
+/* Returns the number of children that oNParent has. */
+size_t dNode_getNumChildren(dNode_T oNParent){
+    return(DynArray_getLength(oNParent->oDChildren));
+}
+/* think i'm done */
+
+
+/*
+  Returns an int SUCCESS status and sets *poNResult to be the child
+  node of oNParent with identifier ulChildID, if one exists.
+  Otherwise, sets *poNResult to NULL and returns status:
+  * NO_SUCH_PATH if ulChildID is not a valid child for oNParent
+*/
+int dNode_getChild(dNode_T oNParent, size_t ulChildID,
+                  dNode_T *poNResult){
+                    if(ulChildID>= DynArray_getLength(oNParent->oDChildren)){
+                        *poNResult = NULL;
+                        return NO_SUCH_PATH;
+                    }
+                    poNResult = DynArray_get(oNParent->oDChildren, ulChildID);
+                    return SUCCESS;
+            }
+/* think i'm done */
+
+
+/*
+  Returns the parent node of oNNode.
+  Returns NULL if oNNode is the root and thus has no parent.
+*/
+dNode_T dNode_getParent(dNode_T oNNode){
+    return(*(oNNode->oNParent));
+}
+/* think i'm done */
+
+/*
+  Compares oNFirst and oNSecond lexicographically based on their paths.
+  Returns <0, 0, or >0 if onFirst is "less than", "equal to", or
+  "greater than" oNSecond, respectively.
+*/
+int dNode_compare(dNode_T oNFirst, dNode_T oNSecond){
+    return (Path_comparePath(oNFirst->oPPath, oNSecond->oPPath));
+}
+/* think i'm done */
+
+/*
+  Returns a string representation for oNNode, or NULL if
+  there is an allocation error.
+
+  Allocates memory for the returned string, which is then owned by
+  the caller!
+*/
+char *dNode_toString(dNode_T oNNode);
---
+++ 3FT/ft.c
@@ -0,0 +1,177 @@
+/*--------------------------------------------------------------------*/
+/* ft.c                                                               */
+/* Author: Judah Guggenheim                                           */
+/*--------------------------------------------------------------------*/
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include "d_nodeFT.h"
+#include "dynarray.h"
+#include "ft.h"
+
+struct ft
+{
+/* a boolean stating whether the FT has been initalized or not. */
+/* This should be FALSE before the FT is initialized. */
+boolean bIsInitialized = FALSE;
+
+/* a directory node that serves as the root of the FT. */
+/* The root should be null before the FT is initialized. */
+dNode_T oNRoot = NULL;
+
+/* ulCount keeps track of how many nodes are in the FT. */
+/* The count should be 0 before the FT is initialized. */
+size_t ulCount = 0;
+};
+
+
+/*
+   Inserts a new directory into the FT with absolute path pcPath.
+   Returns SUCCESS if the new directory is inserted successfully.
+   Otherwise, returns:
+   * INITIALIZATION_ERROR if the FT is not in an initialized state
+   * BAD_PATH if pcPath does not represent a well-formatted path
+   * CONFLICTING_PATH if the root exists but is not a prefix of pcPath
+   * NOT_A_DIRECTORY if a proper prefix of pcPath exists as a file
+   * ALREADY_IN_TREE if pcPath is already in the FT (as dir or file)
+   * MEMORY_ERROR if memory could not be allocated to complete request
+*/
+int FT_insertDir(const char *pcPath){
+    dNode_T *poNResult;
+    Path_T oPPath;
+    dNode_T oNParent;
+    int temp;
+
+    if(bIsInitialized == FALSE) {return INITIALIZATION_ERROR};
+    temp = Path_new(pcPath, oPPath);
+    if (temp != SUCCESS) {return temp;}
+/* get parent from the FT tree itself, using the path */
+
+    return = dNode_new(oPPath, oNParent, poNResult);
+    /*make sure that dNode_new checks if something is being added into a file (not allowed), instead of into a directory (allowed)*/
+}
+
+/*
+  Returns TRUE if the FT contains a directory with absolute path
+  pcPath and FALSE if not or if there is an error while checking.
+*/
+boolean FT_containsDir(const char *pcPath);
+
+/*
+  Removes the FT hierarchy (subtree) at the directory with absolute
+  path pcPath. Returns SUCCESS if found and removed.
+  Otherwise, returns:
+  * INITIALIZATION_ERROR if the FT is not in an initialized state
+  * BAD_PATH if pcPath does not represent a well-formatted path
+  * CONFLICTING_PATH if the root exists but is not a prefix of pcPath
+  * NO_SUCH_PATH if absolute path pcPath does not exist in the FT
+  * NOT_A_DIRECTORY if pcPath is in the FT as a file not a directory
+  * MEMORY_ERROR if memory could not be allocated to complete request
+*/
+int FT_rmDir(const char *pcPath);
+
+
+/*
+   Inserts a new file into the FT with absolute path pcPath, with
+   file contents pvContents of size ulLength bytes.
+   Returns SUCCESS if the new file is inserted successfully.
+   Otherwise, returns:
+   * INITIALIZATION_ERROR if the FT is not in an initialized state
+   * BAD_PATH if pcPath does not represent a well-formatted path
+   * CONFLICTING_PATH if the root exists but is not a prefix of pcPath,
+                      or if the new file would be the FT root
+   * NOT_A_DIRECTORY if a proper prefix of pcPath exists as a file
+   * ALREADY_IN_TREE if pcPath is already in the FT (as dir or file)
+   * MEMORY_ERROR if memory could not be allocated to complete request
+*/
+int FT_insertFile(const char *pcPath, void *pvContents,
+                  size_t ulLength);
+
+/*
+  Returns TRUE if the FT contains a file with absolute path
+  pcPath and FALSE if not or if there is an error while checking.
+*/
+boolean FT_containsFile(const char *pcPath);
+
+/*
+  Removes the FT file with absolute path pcPath.
+  Returns SUCCESS if found and removed.
+  Otherwise, returns:
+  * INITIALIZATION_ERROR if the FT is not in an initialized state
+  * BAD_PATH if pcPath does not represent a well-formatted path
+  * CONFLICTING_PATH if the root exists but is not a prefix of pcPath
+  * NO_SUCH_PATH if absolute path pcPath does not exist in the FT
+  * NOT_A_FILE if pcPath is in the FT as a directory not a file
+  * MEMORY_ERROR if memory could not be allocated to complete request
+*/
+int FT_rmFile(const char *pcPath);
+
+/*
+  Returns the contents of the file with absolute path pcPath.
+  Returns NULL if unable to complete the request for any reason.
+
+  Note: checking for a non-NULL return is not an appropriate
+  contains check, because the contents of a file may be NULL.
+*/
+void *FT_getFileContents(const char *pcPath);
+
+/*
+  Replaces current contents of the file with absolute path pcPath with
+  the parameter pvNewContents of size ulNewLength bytes.
+  Returns the old contents if successful. (Note: contents may be NULL.)
+  Returns NULL if unable to complete the request for any reason.
+*/
+void *FT_replaceFileContents(const char *pcPath, void *pvNewContents,
+                             size_t ulNewLength);
+
+/*
+  Returns SUCCESS if pcPath exists in the hierarchy,
+  Otherwise, returns:
+  * INITIALIZATION_ERROR if the FT is not in an initialized state
+  * BAD_PATH if pcPath does not represent a well-formatted path
+  * CONFLICTING_PATH if the root's path is not a prefix of pcPath
+  * NO_SUCH_PATH if absolute path pcPath does not exist in the FT
+  * MEMORY_ERROR if memory could not be allocated to complete request
+
+  When returning SUCCESS,
+  if path is a directory: sets *pbIsFile to FALSE, *pulSize unchanged
+  if path is a file: sets *pbIsFile to TRUE, and
+                     sets *pulSize to the length of file's contents
+
+  When returning another status, *pbIsFile and *pulSize are unchanged.
+*/
+int FT_stat(const char *pcPath, boolean *pbIsFile, size_t *pulSize);
+
+/*
+  Sets the FT data structure to an initialized state.
+  The data structure is initially empty.
+  Returns INITIALIZATION_ERROR if already initialized,
+  and SUCCESS otherwise.
+*/
+int FT_init(void){
+
+}
+
+/*
+  Removes all contents of the data structure and
+  returns it to an uninitialized state.
+  Returns INITIALIZATION_ERROR if not already initialized,
+  and SUCCESS otherwise.
+*/
+int FT_destroy(void);
+
+/*
+  Returns a string representation of the
+  data structure, or NULL if the structure is
+  not initialized or there is an allocation error.
+
+  The representation is depth-first with files
+  before directories at any given level, and nodes
+  of the same type ordered lexicographically.
+
+  Allocates memory for the returned string,
+  which is then owned by client!
+*/
+char *FT_toString(void);
